f6fcb41fe9397026357b77fa0a9841f58f73a0f0
Justin Angevaare <justin@Justins-MacBook-Pro.local>
2017-02-08 21:32:52 -0500

    Fix efficiency calculation and add linear equation

--- app.R
+++ app.R
@@ -40,39 +40,50 @@ ui = fluidPage(
 						min = 0.05,
 						max = 0.15,
 						value = 0.100,
-						step = 0.01),
+						step = 0.01),					
 			sliderInput("converteff",
-						"Conversion efficiency (%)",
-						min = 50,
-						max = 100,
-						value = 95,
-						step = 1)
+						"Conversion efficiency",
+						min = 0.50,
+						max = 1.00,
+						value = 0.95,
+						step = 0.01)
+					
 			),	
 		mainPanel(
-			plotOutput("effPlot", height="400px", width="600px")
+			plotOutput("effPlot", height="400px", width="600px"),
+			br(),
+			p("You can approximate your brewhouse efficiency with the following linear equation:"),
+			h4(textOutput("equation"))
 			)
 		)
 	)
 
 server = function(input, output){
 	brewcalc = function(){
-		grain = seq(floor(0.5*input$batchsize), ceiling(4*input$batchsize))
-		shrinkage = 4
-		postboilvol = ((input$batchsize + input$deadloss1a) * (100/(100-shrinkage))) + input$deadloss1b
+		t100density = 0.960
+		ppg = 1.030
+		grain = seq(floor(0.75*input$batchsize), ceiling(3*input$batchsize))
+		totalsugar = (ppg - 1)*grain
+		postboilvol = ((input$batchsize + input$deadloss1a) * (1.000/t100density)) + input$deadloss1b
 		preboilvol = postboilvol + input$boiloff
 		totalwater = preboilvol + input$deadloss2 + (input$retention * grain)
-		lauterefficiency = preboilvol / totalwater
-		mashefficiency = lauterefficiency*(input$converteff/100)
-		brewhouseefficiency = mashefficiency * ((input$batchsize * (100/(100-shrinkage))) / postboilvol)
+		preboilgrav = 1 + (totalsugar * input$converteff / totalwater)
+		postboilgrav = 1 + (preboilgrav - 1) * preboilvol/postboilvol
+		brewhouseefficiency = (postboilgrav - 1) * input$batchsize / totalsugar
 		data.frame(Grain=grain, Efficiency=brewhouseefficiency*100)
 	}
 	brewx = reactive(brewcalc())
+	brewlm = reactive(lm(Efficiency~Grain, data=brewx()))
 	output$effPlot = renderPlot({
 		ggplot(brewx(), aes(x=Grain, y=Efficiency)) + 
 			geom_line(size=3) + 
 			labs(x="Total grain bill (lbs)", y="Brewhouse efficiency (%)") +
 			scale_y_continuous(limits = c(40, 100))
 	})
+	output$equation = renderText({
+		paste("Efficiency = ", round(brewlm()$coef[2], 4), " x Grain (lbs) + ", round(brewlm()$coef[1], 4))
+	})
+
 }
 
 shinyApp(ui, server)
